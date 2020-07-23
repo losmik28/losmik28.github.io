478dc927e5a72d9e1d0323ee34b09bdc426c1e60
losmik28 <losmik28@gmail.com>
2020-07-24 00:59:35 +0200

    Added new project

---
+++ 05 - 23.07.2020/script.js
@@ -0,0 +1,50 @@
+// --->>> ZADATAK 1 <<<---
+
+const currentK = 273.15;
+let f = 90;
+
+let c = (f - 32) * 5 / 9;
+// console.log(c);
+let kelvin = c + currentK;
+let k = kelvin;
+// console.log(k);
+
+// Konverzija f u kelvin
+let farToKel = (f - 32) / 1.8 + currentK;
+console.log(farToKel);
+
+// Konverzija kelvin u f
+let kelToFar = (k - currentK) * 1.8 + 32;
+console.log(kelToFar);
+
+
+
+
+// --->>> ZADATAK 2 <<<---
+
+let poglavlja = 15;
+let prviDan = 5;
+let drugiDan = prviDan + 2;
+let ostalo = poglavlja - drugiDan;
+
+console.log(`Ostalo je da procita jos ${ostalo} poglavlja.`);
+
+
+
+
+// --->>> ZADATAK 3 <<<---
+
+let pera = 2000;
+let mika = 3000;
+let ukupanKusur = 1500;
+
+let placeno = pera + mika - ukupanKusur;
+let cenaDzempera = placeno / 2;
+// console.log(cenaDzempera);
+
+console.log(`Placeno je u prodavnici ${placeno} dinara. Dobili su kusur od ${ukupanKusur} dinara.`);
+
+let kusurMika = mika - cenaDzempera;
+let kusurPera = pera - cenaDzempera;
+// console.log(kusurMika, kusurPera);
+console.log(`Mika treba da dobije kusur od ${kusurMika} dinara, a Pera treba da dobije ${kusurPera} dinara.`);

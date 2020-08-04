8fbd0a83461de3576449a1f69e33122649320b8a
losmik28 <losmik28@gmail.com>
2020-08-04 18:14:31 +0200

    Added 08 Homework

---
+++ 08 - 04.08.2020/script.js
@@ -0,0 +1,46 @@
+console.log(`---> ZADATAK 1 <---`);
+
+// ZADATAK 1
+function suma(n, m){
+    let suma1 = 1;
+    for(let i = n; i <= m; i++){
+        suma1 = i ** 3;
+    }
+    return suma1;
+}
+
+let rezultat = suma(2, 4);
+console.log(`Rezultat je: ${rezultat}.`);
+
+console.log(`---> ZADATAK 2 <---`);
+
+
+// ZADATAK 2
+
+function godine(godRodjenja){
+    let vreme = new Date();
+    let trenutnaGod = vreme.getFullYear();
+
+    if(trenutnaGod > godRodjenja){
+        return trenutnaGod - godRodjenja;
+    }
+}
+
+let ukupnoGod = godine(1998);
+console.log(`Milos trenutno ima ${ukupnoGod} godina.`);
+
+console.log(`---> ZADATAK 3 <---`);
+
+// ZADATAK 3
+
+function deljiv(n, m, k){
+    let brojac = 0;
+    for(let i = n; i <= m; i++){
+        if(i % k == 0){
+            console.log(`Broj ${i} je deljiv sa brojem ${k}.`);
+            brojac++;
+        }
+    }
+}
+
+deljiv(90, 100, 3);

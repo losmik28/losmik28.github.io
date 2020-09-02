eb85117780d12922973e057cb78a03e170a9665e
losmik28 <losmik28@gmail.com>
2020-09-02 12:37:38 +0200

    Memory Game project

---
+++ script.js
@@ -0,0 +1,730 @@
+let cardsArray4x4 = [
+    {
+    'name': 'airbaloon',
+    'img': 'images/airbaloon.png',
+    },
+    {
+    'name': 'android',
+    'img': 'images/android.png',
+    },
+    {
+    'name': 'arcade',
+    'img': 'images/arcade.png',
+    },
+    {
+    'name': 'basket',
+    'img': 'images/basket.png',
+    },
+    {
+    'name': 'bean',
+    'img': 'images/bean.png',
+    },
+    {
+    'name': 'bee',
+    'img': 'images/bee.png',
+    },
+    {
+    'name': 'boat',
+    'img': 'images/boat.png',
+    },
+    {
+    'name': 'bones',
+    'img': 'images/bones.png',
+    },
+];
+
+let cardsArray6x6 = [
+    {
+    'name': 'airbaloon',
+    'img': 'images/airbaloon.png',
+    },
+    {
+    'name': 'android',
+    'img': 'images/android.png',
+    },
+    {
+    'name': 'arcade',
+    'img': 'images/arcade.png',
+    },
+    {
+    'name': 'basket',
+    'img': 'images/basket.png',
+    },
+    {
+    'name': 'bean',
+    'img': 'images/bean.png',
+    },
+    {
+    'name': 'bee',
+    'img': 'images/bee.png',
+    },
+    {
+    'name': 'boat',
+    'img': 'images/boat.png',
+    },
+    {
+    'name': 'bones',
+    'img': 'images/bones.png',
+    },
+    {
+    'name': 'chat',
+    'img': 'images/chat.png',
+    },
+    {
+    'name': 'christian-love',
+    'img': 'images/christian-love.png',
+    },
+    {
+    'name': 'clap',
+    'img': 'images/clap.png',
+    },
+    {
+    'name': 'cop-male',
+    'img': 'images/cop-male.png',
+    },
+    {
+    'name': 'cucumber',
+    'img': 'images/cucumber.png',
+    },
+    {
+    'name': 'facebook',
+    'img': 'images/facebook.png',
+    },
+    {
+    'name': 'family-institution',
+    'img': 'images/family-institution.png',
+    },
+    {
+    'name': 'fear',
+    'img': 'images/fear.png',
+    },
+    {
+    'name': 'flag',
+    'img': 'images/flag.png',
+    },
+    {
+    'name': 'gift',
+    'img': 'images/gift.png',
+    },
+];
+
+let cardsArray8x8 = [
+    {
+    'name': 'airbaloon',
+    'img': 'images/airbaloon.png',
+    },
+    {
+    'name': 'android',
+    'img': 'images/android.png',
+    },
+    {
+    'name': 'arcade',
+    'img': 'images/arcade.png',
+    },
+    {
+    'name': 'basket',
+    'img': 'images/basket.png',
+    },
+    {
+    'name': 'bean',
+    'img': 'images/bean.png',
+    },
+    {
+    'name': 'bee',
+    'img': 'images/bee.png',
+    },
+    {
+    'name': 'boat',
+    'img': 'images/boat.png',
+    },
+    {
+    'name': 'bones',
+    'img': 'images/bones.png',
+    },
+    {
+    'name': 'chat',
+    'img': 'images/chat.png',
+    },
+    {
+    'name': 'christian-love',
+    'img': 'images/christian-love.png',
+    },
+    {
+    'name': 'clap',
+    'img': 'images/clap.png',
+    },
+    {
+    'name': 'cop-male',
+    'img': 'images/cop-male.png',
+    },
+    {
+    'name': 'cucumber',
+    'img': 'images/cucumber.png',
+    },
+    {
+    'name': 'facebook',
+    'img': 'images/facebook.png',
+    },
+    {
+    'name': 'family-institution',
+    'img': 'images/family-institution.png',
+    },
+    {
+    'name': 'fear',
+    'img': 'images/fear.png',
+    },
+    {
+    'name': 'flag',
+    'img': 'images/flag.png',
+    },
+    {
+    'name': 'gift',
+    'img': 'images/gift.png',
+    },
+    {
+    'name': 'grave',
+    'img': 'images/grave.png',
+    },
+    {
+    'name': 'heart-hand',
+    'img': 'images/heart-hand.png',
+    },
+    {
+    'name': 'knight',
+    'img': 'images/knight.png',
+    },
+    {
+    'name': 'link',
+    'img': 'images/link.png',
+    },
+    {
+    'name': 'macpro',
+    'img': 'images/macpro.png',
+    },
+    {
+    'name': 'minion-batman',
+    'img': 'images/minion-batman.png',
+    },
+    {
+    'name': 'mirror',
+    'img': 'images/mirror.png',
+    },
+    {
+    'name': 'mobile',
+    'img': 'images/mobile.png',
+    },
+    {
+    'name': 'monkey-silent',
+    'img': 'images/monkey-silent.png',
+    },
+    {
+    'name': 'octopus-angry',
+    'img': 'images/octopus-angry.png',
+    },
+    {
+    'name': 'parka',
+    'img': 'images/parka.png',
+    },
+    {
+    'name': 'pig',
+    'img': 'images/pig.png',
+    },
+    {
+    'name': 'planets',
+    'img': 'images/planets.png',
+    },
+    {
+    'name': 'pluger',
+    'img': 'images/pluger.png',
+    },
+];
+
+let cardsArray10x10 = [
+    {
+    'name': 'airbaloon',
+    'img': 'images/airbaloon.png',
+    },
+    {
+    'name': 'android',
+    'img': 'images/android.png',
+    },
+    {
+    'name': 'arcade',
+    'img': 'images/arcade.png',
+    },
+    {
+    'name': 'basket',
+    'img': 'images/basket.png',
+    },
+    {
+    'name': 'bean',
+    'img': 'images/bean.png',
+    },
+    {
+    'name': 'bee',
+    'img': 'images/bee.png',
+    },
+    {
+    'name': 'boat',
+    'img': 'images/boat.png',
+    },
+    {
+    'name': 'bones',
+    'img': 'images/bones.png',
+    },
+    {
+    'name': 'chat',
+    'img': 'images/chat.png',
+    },
+    {
+    'name': 'christian-love',
+    'img': 'images/christian-love.png',
+    },
+    {
+    'name': 'clap',
+    'img': 'images/clap.png',
+    },
+    {
+    'name': 'cop-male',
+    'img': 'images/cop-male.png',
+    },
+    {
+    'name': 'cucumber',
+    'img': 'images/cucumber.png',
+    },
+    {
+    'name': 'facebook',
+    'img': 'images/facebook.png',
+    },
+    {
+    'name': 'family-institution',
+    'img': 'images/family-institution.png',
+    },
+    {
+    'name': 'fear',
+    'img': 'images/fear.png',
+    },
+    {
+    'name': 'flag',
+    'img': 'images/flag.png',
+    },
+    {
+    'name': 'gift',
+    'img': 'images/gift.png',
+    },
+    {
+    'name': 'grave',
+    'img': 'images/grave.png',
+    },
+    {
+    'name': 'heart-hand',
+    'img': 'images/heart-hand.png',
+    },
+    {
+    'name': 'knight',
+    'img': 'images/knight.png',
+    },
+    {
+    'name': 'link',
+    'img': 'images/link.png',
+    },
+    {
+    'name': 'macpro',
+    'img': 'images/macpro.png',
+    },
+    {
+    'name': 'minion-batman',
+    'img': 'images/minion-batman.png',
+    },
+    {
+    'name': 'mirror',
+    'img': 'images/mirror.png',
+    },
+    {
+    'name': 'mobile',
+    'img': 'images/mobile.png',
+    },
+    {
+    'name': 'monkey-silent',
+    'img': 'images/monkey-silent.png',
+    },
+    {
+    'name': 'octopus-angry',
+    'img': 'images/octopus-angry.png',
+    },
+    {
+    'name': 'parka',
+    'img': 'images/parka.png',
+    },
+    {
+    'name': 'pig',
+    'img': 'images/pig.png',
+    },
+    {
+    'name': 'planets',
+    'img': 'images/planets.png',
+    },
+    {
+    'name': 'pluger',
+    'img': 'images/pluger.png',
+    },
+    {
+    'name': 'polygon',
+    'img': 'images/polygon.png',
+    },
+    {
+    'name': 'potion',
+    'img': 'images/potion.png',
+    },
+    {
+    'name': 'predator',
+    'img': 'images/predator.png',
+    },
+    {
+    'name': 'puzzle',
+    'img': 'images/puzzle.png',
+    },
+    {
+    'name': 'search',
+    'img': 'images/search.png',
+    },
+    {
+    'name': 'shit-sunglasses',
+    'img': 'images/shit-sunglasses.png',
+    },
+    {
+    'name': 'short-hair',
+    'img': 'images/short-hair.png',
+    },
+    {
+    'name': 'spray',
+    'img': 'images/spray.png',
+    },
+    {
+    'name': 'student-female',
+    'img': 'images/student-female.png',
+    },
+    {
+    'name': 'sun',
+    'img': 'images/sun.png',
+    },
+    {
+    'name': 'topview',
+    'img': 'images/topview.png',
+    },
+    {
+    'name': 'troller',
+    'img': 'images/troller.png',
+    },
+    {
+    'name': 'twitter',
+    'img': 'images/twitter.png',
+    },
+    {
+    'name': 'umbrella-rain',
+    'img': 'images/umbrella-rain.png',
+    },
+    {
+    'name': 'vault',
+    'img': 'images/vault.png',
+    },
+    {
+    'name': 'vision-jew',
+    'img': 'images/vision-jew.png',
+    },
+    {
+    'name': 'walle-female',
+    'img': 'images/walle-female.png',
+    },
+    {
+    'name': 'water-bottle',
+    'img': 'images/water-bottle.png',
+    },
+];
+
+let inputUsername = document.getElementById("username");
+let section = document.querySelector("grid");
+let easyRadio = document.querySelector("#lako");
+let mediumRadio = document.querySelector("#srednje");
+let hardRadio = document.querySelector("#tesko");
+let expertRadio = document.querySelector("#ekspert");
+let timer = document.querySelector("#timer");
+let usernames = document.querySelector(".usernamePlayer");
+
+let counterTimer = null;
+let izanbraniNiz = null;
+let izabranaKlasa = null;
+let c = 0;
+let prviKlik = true;
+let counterTime = 0;
+
+let userScore = {};
+let easyScore = [];
+let mediumScore = [];
+let hardScore = [];
+let expertScore = [];
+let myArr = [];
+
+// console.log(JSON.parse(localStorage.getItem("playerUsername")));
+if(JSON.parse(localStorage.getItem("arrScoreEasy")) == null){
+    localStorage.setItem("arrScoreEasy", JSON.stringify(easyScore));
+} else {
+    easyScore = JSON.parse(localStorage.getItem("arrScoreEasy"))
+}
+if(JSON.parse(localStorage.getItem("arrScoreMedium")) == null){
+    localStorage.setItem("arrScoreMedium", JSON.stringify(mediumScore));
+} else {
+    mediumScore = JSON.parse(localStorage.getItem("arrScoreMedium"))
+}
+if(JSON.parse(localStorage.getItem("arrScoreHard")) == null){
+    localStorage.setItem("arrScoreHard", JSON.stringify(hardScore));
+} else {
+    hardScore = JSON.parse(localStorage.getItem("arrScoreHard"))
+}
+if(JSON.parse(localStorage.getItem("arrScoreExpert")) == null){
+    localStorage.setItem("arrScoreExpert", JSON.stringify(expertScore));
+} else {
+    expertScore = JSON.parse(localStorage.getItem("arrScoreExpert"))
+}
+
+let user = localStorage.getItem('playerUsername');
+inputUsername.value = user;
+
+inputUsername.addEventListener('keyup', (event) => {
+    if(event.keyCode == 13){
+        if(inputUsername.value == "" || inputUsername.value == null){
+            alert("Please enter your username, otherwise you will not be able to start the game!");
+        } else {
+            localStorage.setItem("playerUsername", inputUsername.value);
+        }
+    }
+});
+
+
+function newBoard(arr, klasa){
+    
+    let gameGrid = arr.concat(arr).sort((arr) => 0.5 - Math.random());
+
+    let firstGuess = '';
+    let secondGuess = '';
+    let count = 0;
+    let previousTarget = null;
+    let delay = 700;
+
+    let game = document.getElementById('game');
+    game.innerHTML="";
+    let gridSection = document.createElement('section');
+    gridSection.setAttribute(`class`, `${klasa}`);
+    game.appendChild(gridSection);
+
+    gameGrid.forEach(item => {
+        let { name, img } = item;
+
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.name = name;
+
+        let front = document.createElement('div');
+        front.classList.add('front');
+
+        let back = document.createElement('div');
+        back.classList.add('back');
+        back.style.backgroundImage = `url(${img})`;
+
+        gridSection.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
+    });
+
+    function match(){
+        let selected = document.querySelectorAll('.selected');
+        selected.forEach(card => {
+            card.classList.add('match');
+        });
+    };
+    
+    function resetGuesses(){
+        firstGuess = '';
+        secondGuess = '';
+        count = 0;
+        previousTarget = null;
+    
+        let selected = document.querySelectorAll('.selected');
+        selected.forEach(card => {
+            card.classList.remove('selected');
+        });
+    };
+
+    gridSection.addEventListener('click', event => {
+        if(prviKlik == true){
+            if(counterTimer === null){
+
+                let rb = document.querySelector("input[type='radio']:checked");
+                let rbs = document.querySelectorAll("input[type='radio']");
+
+                rbs.forEach(r => {
+                    if(r != rb){
+                        r.disabled = true;
+                    }
+                });
+                counterTimer = setInterval(() => {
+                    timer.value = Number(timer.value) + 1;
+                }, 1000);
+            }
+            prviKlik = false;
+        }
+
+        let clicked = event.target;
+
+        if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') ||
+        clicked.parentNode.classList.contains('match')){
+            return;
+        }
+        if(count < 2){
+            count++;
+            if(count === 1){
+                firstGuess = clicked.parentNode.dataset.name;
+                // console.log(firstGuess);
+                clicked.parentNode.classList.add('selected');
+            } else{
+                secondGuess = clicked.parentNode.dataset.name;
+                // console.log(secondGuess);
+                clicked.parentNode.classList.add('selected');
+            }
+
+            if(firstGuess && secondGuess){
+                if(firstGuess === secondGuess){
+                    setTimeout(match, delay);
+                    c += 2;
+                    if(c == izanbraniNiz.length * 2){
+                        let radio1 = document.querySelector("input[type='radio']:checked");
+                        userScore = {
+                            name: inputUsername.value,
+                            time: Number(timer.value),
+                            nivo: radio1.id
+                        }
+                        switch(radio1.id){
+                            case "lako":
+                                easyScore.push(userScore);
+                                easyScore.sort(function (a, b) {
+                                    return a.time - b.time;
+                                });
+                                let topFiveScoresEasy = easyScore.slice(0, 5);
+                                console.log(topFiveScoresEasy);
+                                localStorage.setItem("arrScoreEasy", JSON.stringify(topFiveScoresEasy));
+                                break;
+                            case "srednje":
+                                mediumScore.push(userScore);
+                                mediumScore.sort(function (a, b) {
+                                    return a.time - b.time;
+                                });
+                                let topFiveScoresMedium = mediumScore.slice(0, 5);
+                                localStorage.setItem("arrScoreMedium", JSON.stringify(topFiveScoresMedium));
+                                break;
+                            case "tesko":
+                                hardScore.push(userScore);
+                                hardScore.sort(function (a, b) {
+                                    return a.time - b.time;
+                                });
+                                let topFiveScoresHard = hardScore.slice(0, 5);
+                                localStorage.setItem("arrScoreHard", JSON.stringify(topFiveScoresHard));
+                                break;
+                            case "ekspert":
+                                expertScore.push(userScore);
+                                expertScore.sort(function (a, b) {
+                                    return a.time - b.time;
+                                });
+                                let topFiveScoresExpert = expertScore.slice(0, 5);
+                                localStorage.setItem("arrScoreExpert", JSON.stringify(topFiveScoresExpert));
+                                break;
+                        }
+                        
+                        if(confirm(`Congratilations ${inputUsername.value}, your time: ${timer.value}s, play again?`)){
+                            let board = document.getElementById('game');
+                            board.innerHTML = "";
+                            timer.value = "0";
+                            clearInterval(counterTimer);
+                            counterTimer = null;
+                            prviKlik = true;
+                            let rbs = document.querySelectorAll("input[type='radio']");     
+                            rbs.forEach(r => {
+                                r.disabled=false;
+                            });
+
+                            newBoard(izanbraniNiz, izabranaKlasa);
+                        } else {
+                            clearInterval(counterTimer);
+                            // timer.value = "0";
+                            let rbs = document.querySelectorAll("input[type='radio']");     
+                            rbs.forEach(r => {
+                                r.disabled=false;
+                            });
+                        }
+                    }
+                }
+                setTimeout(resetGuesses, delay);
+            }
+            previousTarget = clicked;
+        }
+
+    });
+}
+
+function tableHighScore(arr){
+    let divHighScore = document.getElementById('tableBestScores');
+    let tableHs = `<table class="col-m-12 col-t-12 col-12" id="scores">`;
+    for(let i = 0; i < 1; i++){
+        tableHs += `<tr><td id="place">Place</td><td id="username">Username</td><td id="time">Time</td></tr>`;
+        for(let j = 0; j < arr.length; j++){
+            tableHs += `<tr class="tableResults"><td>${j + 1}</td><td>${arr[j].name}</td><td>${arr[j].time}</td></tr>`;
+        }
+    }
+    tableHs += `</table>`;
+    divHighScore.innerHTML = tableHs;
+}
+
+easyRadio.addEventListener("click", () => {
+    newBoard(cardsArray4x4, "grid4x4");
+    izanbraniNiz = cardsArray4x4;
+    izabranaKlasa = "grid4x4";
+});
+mediumRadio.addEventListener("click", () => {
+    newBoard(cardsArray6x6, "grid6x6");
+    izanbraniNiz = cardsArray6x6;
+    izabranaKlasa="grid6x6";
+
+});
+hardRadio.addEventListener("click", () => {
+    newBoard(cardsArray8x8, "grid8x8");
+    izanbraniNiz = cardsArray8x8;
+    izabranaKlasa = "grid8x8";
+
+});
+expertRadio.addEventListener("click", () => {
+    newBoard(cardsArray10x10, "grid10x10");
+    izanbraniNiz = cardsArray10x10;
+    izabranaKlasa = "grid10x10";
+});
+
+let liEasyValue = document.getElementById("easyTable");
+let liMediumValue = document.getElementById("mediumTable");
+let liHardValue = document.getElementById("hardTable");
+let liExpertValue = document.getElementById("veryHardTable");
+let tableSpan = document.getElementById("spanTable");
+
+liEasyValue.addEventListener('click', () => {
+    tableSpan.innerHTML = liEasyValue.textContent;
+    tableHighScore(easyScore);
+});
+liMediumValue.addEventListener('click', () => {
+    tableSpan.innerHTML = liMediumValue.textContent;
+    tableHighScore(mediumScore);
+});
+liHardValue.addEventListener('click', () => {
+    tableSpan.innerHTML = liHardValue.textContent;
+    tableHighScore(hardScore);
+});
+liExpertValue.addEventListener('click', () => {
+    tableSpan.innerHTML = liExpertValue.textContent;
+    tableHighScore(expertScore);
+});
